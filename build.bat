d7b606eb934240bad44dc3dfe2d60e4697f156bb
thirdplay <chronokreuz@gmail.com>
2016-03-06 19:22:12 +0900

    templateを考慮したcombine/decombine処理の拡張

--- build.bat
+++ build.bat
@@ -1,12 +1,6 @@
 @echo off
 cd %~dp0
 
-@rem テンプレートファイルをbinディレクトリにコピーする
-if not exist bin mkdir bin
-cd src
-for /d %%A in (*.xlsm) do copy %%A\template.xlsm ..\bin\%%A >nul
-cd ..
-
 @rem 結合処理実行
-cscript //nologo vbac.wsf combine
+cscript //nologo vbac.wsf combine /template
 if not %ERRORLEVEL%==0 pause
--- decombine.bat
+++ decombine.bat
@@ -1,22 +1,6 @@
 @echo off
-set BIN_DIR=bin
-set BAK_DIR=bak%DATE:/=%
 cd %~dp0
 
-@rem 一時ディレクトリ作成
-mkdir %BAK_DIR%
-copy %BIN_DIR%\* %BAK_DIR% >nul
-
 @rem 分離処理実行
-cscript //nologo vbac.wsf decombine /binary %BAK_DIR%
+cscript //nologo vbac.wsf decombine /template
 if not %ERRORLEVEL%==0 pause
-cscript //nologo vbac.wsf clear /binary %BAK_DIR%
-if not %ERRORLEVEL%==0 pause
-
-@rem bak/*.xlsmをsrcにコピー
-cd %BAK_DIR%
-for %%A in (*.xlsm) do copy %%A ..\src\%%A\template.xlsm >nul
-cd ..
-
-@rem 一時ディレクトリ削除
-rmdir /s /q %BAK_DIR%
